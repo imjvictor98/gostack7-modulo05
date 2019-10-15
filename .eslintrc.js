fb3c304dc339ad175779853825f9b7d3ca034fc6
imjvictor98 <jv.camara@hotmail.com>
2019-10-15 11:07:11 -0300

    Airbnb eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': [0, 'always'], // para poder usar o state no modo antigo, fora do construtor
+    'react/prop-types': [0], // para ignorar os erros de chamada a props no modo antigo
+    'react/static-property-placement': [0], // para ignorar os proptypes definidos no modo antigo
+  },
+};
